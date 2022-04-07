f173904d8cdf0c367fcc6581b5b211abdc6610d4
Dinaerte Neto <dneto@redventures.com>
2022-04-07 08:23:14 -0300

    theme change using provider

---
+++ src/theme/palette.js
@@ -0,0 +1,15 @@
+import { colors } from "@material-ui/core"
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    primary: {
+        dark: colors.indigo['A700'],
+        main: colors.indigo['A400'],
+        light: colors.indigo['A200']
+    },
+    secondary: {
+        dark: colors.orange['A700'],
+        main: colors.orange['A400'],
+        light: colors.orange['A200']
+    }
+}
---
+++ src/theme/typography.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    h6: {
+        fontSize: 14,
+        fontWeight: 500,
+        letterSpacing: '-0.05px',
+    }
+}

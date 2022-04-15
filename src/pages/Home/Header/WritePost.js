3422ed74e3be628343ceb37732aa1711fcdbe949
Dinaerte Neto <dinaerteneto@gmail.com>
2022-04-15 07:24:05 -0300

    verify user is authenticated in writePost

--- src/pages/Home/Header/WritePost.js
+++ src/pages/Home/Header/WritePost.js
@@ -1,10 +1,20 @@
 import React from "react"
+import { useSelector } from "react-redux";
 import Button from "@material-ui/core/Button"
 import { useNavigate } from "react-router-dom";
 
 function WritePost() {
+    const user = useSelector(state => state.account.user);
+    const isAuthenticated = !!user
     const navigate = useNavigate()
-    const toNewPost = () => navigate('/post/new')
+
+    const toNewPost = () => {
+        if (isAuthenticated) {
+            navigate('/post/new')
+        } else {
+            navigate('/sign-in')
+        }
+    }
 
     return (
         <Button color="primary" variant="contained" onClick={toNewPost}>

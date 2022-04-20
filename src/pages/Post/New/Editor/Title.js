ed248160e872d0dcc888a4a44225d6dcebf45076
Dinaerte Neto <dinaerteneto@gmail.com>
2022-04-20 05:37:47 -0300

    fix

--- src/pages/Post/New/Editor/Title.js
+++ src/pages/Post/New/Editor/Title.js
@@ -3,7 +3,7 @@ import { TextField } from "@material-ui/core"
 import { usePost } from "../../../../context/PostContext"
 
 function Title() {
-    const ctx = usePost(PostContext)
+    const ctx = usePost()
     const { title, setTitle } = ctx
     return (
         <TextField name="title" placeholder="Título" fullWidth value={title} onChange={setTitle} />
